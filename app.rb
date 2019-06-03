fc2c1c9ac3676c932f5db7eb599ac0c600983363
efrj <efrj@hotmail.com>
2019-06-03 15:34:24 -0300

    Adding app.rb (core application)

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'ramaze'
+
+class PageController < Ramaze::Controller
+  def index
+  	@title = 'table'
+    @colors = ['#cb3d00', '#336699', '#521515', '#478244', '#8f2525', '#4e5b6e', '#ff5a36', '#004b26', '#295398', '#6f0000']
+    render_file 'views/index.xhtml', :title => @title, :colors => @colors
+  end
+end
+
+Ramaze.start
