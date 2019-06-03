7de83dc2fed095f7be890c1974543dc85e30d7a7
efrj <efrj@hotmail.com>
2019-06-03 16:08:37 -0300

    Updated ruby.app

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'ramaze'
 
 class PageController < Ramaze::Controller
   def index
-  	@title = 'table'
+  	@title = 'Tabuada Ruby'
     @colors = ['#cb3d00', '#336699', '#521515', '#478244', '#8f2525', '#4e5b6e', '#ff5a36', '#004b26', '#295398', '#6f0000']
     render_file 'views/index.xhtml', :title => @title, :colors => @colors
   end
